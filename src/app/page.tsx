bbfa356965ce75ba5fd9d98ce0b88ea2448e624b
arya <aryapawar92@gmail.com>
2025-04-05 21:05:45 +0530

    cleaning code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,11 +18,25 @@ export default function Home() {
     <div
       className={` ${space.className} flex flex-col justify-center items-center h-screen bg-black text-white`}
     >
-      <motion.h1 whileInView={{}} className="text-7xl m-4">
+      <motion.h1 whileInView={{ scale: 1.1 }} className="text-7xl m-4">
         Chat App
       </motion.h1>
-      <h4 className="text-2xl m-2">by Arya Pawar</h4>
-      <h3 className="text-2xl m-2">Talk to Anyone. Anywhere. Anytime</h3>
+      <motion.h4
+        initial={{ x: -100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="text-2xl m-2"
+      >
+        by Arya Pawar
+      </motion.h4>
+      <motion.h3
+        initial={{ x: 100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="text-2xl m-2"
+      >
+        Talk to Anyone. Anywhere. Anytime
+      </motion.h3>
       <button
         className="p-2 mt-4 border border-white rounded-xl hover:bg-white hover:text-black cursor-pointer"
         onClick={goToLogin}
